f2bd0c0099977de8488f153e683dd0a65d81b987
Joseph Knowles <joseph@josephknowles.com>
2019-05-03 21:59:35 +0100

    Added a basic demo

---
+++ RestfulDemo/BasicRequest.cs
@@ -0,0 +1,39 @@
+﻿namespace RestfulDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using RestSharp;
+
+    internal class BasicRequest
+    {
+        internal void DemoBasicRequest()
+        {
+            var restClientUri = "https://api.github.com";
+            var restRequestContent = "/repos/simpleinjector/simpleinjector/releases";
+
+            Console.WriteLine(
+                $"This function queries the github api '{restClientUri}' to get the release for the repository found at '{restRequestContent}.'");
+            Console.WriteLine();
+
+            var client = new RestClient(restClientUri);
+            var request = new RestRequest(restRequestContent, Method.GET, DataFormat.Json);
+            var responseData = client.Execute<List<Release>>(request, Method.GET);
+
+            foreach (var release in responseData.Data)
+                Console.WriteLine(
+                    $"\tTag {release.Tag_Name} and created {release.Created_At} by {release.Author.Login}");
+        }
+
+        internal class Release
+        {
+            public string Tag_Name { get; set; }
+            public string Created_At { get; set; }
+            public Author Author { get; set; }
+        }
+
+        internal class Author
+        {
+            public string Login { get; set; }
+        }
+    }
+}
---
+++ RestfulDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RestfulDemo
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("REST API Demo - using RestSharp");
+
+            // Basic - write this one in blue
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            new BasicRequest().DemoBasicRequest();
+            Console.WriteLine();
+
+
+            // Ask for exit so that data can be read by the user
+            Console.ResetColor();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
